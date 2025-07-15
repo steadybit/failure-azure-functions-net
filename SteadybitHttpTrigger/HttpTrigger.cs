786c97ef0e7d597ba0df495c843a79e4c2afc35d
Aleksa Bajat <aleksabajat15@gmail.com>
2025-07-15 12:33:15 +0200

    feat: show fault configuration on GET

--- SteadybitHttpTrigger/HttpTrigger.cs
+++ SteadybitHttpTrigger/HttpTrigger.cs
@@ -1,16 +1,21 @@
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SteadybitFaultInjection;
 
 namespace Steadybit.HttpTrigger;
 
 public class HttpTrigger
 {
     private readonly ILogger<HttpTrigger> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HttpTrigger(ILogger<HttpTrigger> logger)
+    public HttpTrigger(ILogger<HttpTrigger> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [Function("HttpTrigger")]
@@ -18,11 +23,11 @@ public class HttpTrigger
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req
     )
     {
+        var options = new SteadybitInjectionOptions();
+        _configuration.GetSection("Steadybit:FaultInjection").Bind(options);
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-        await response.WriteStringAsync(
-            "Hello, this is a response from the HTTP trigger function!"
-        );
+        await response.WriteStringAsync(JsonSerializer.Serialize(options));
         return response;
     }
 }
